cdc1a05fad97427bf9f566a28ad55f2455852ccb
Khoby-790 <ebaidoo79@gmail.com>
2021-07-05 17:08:50 +0000

    Modified src/layout/components/Details.jsx

--- src/layout/components/Details.jsx
+++ src/layout/components/Details.jsx
@@ -8,6 +8,34 @@ import {
     PlusIcon as PlusIconSolid,
 } from '@heroicons/react/solid'
 
+const currentFile = {
+    name: 'IMG_4985.HEIC',
+    size: '3.9 MB',
+    source:
+        'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+    information: {
+        'Uploaded by': 'Marie Culver',
+        Created: 'June 8, 2020',
+        'Last modified': 'June 8, 2020',
+        Dimensions: '4032 x 3024',
+        Resolution: '72 x 72',
+    },
+    sharedWith: [
+        {
+            id: 1,
+            name: 'Aimee Douglas',
+            imageUrl:
+                'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=1024&h=1024&q=80',
+        },
+        {
+            id: 2,
+            name: 'Andrea McMillan',
+            imageUrl:
+                'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=oilqXxSqey&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    ],
+}
+
 
 const Details = () => {
     return (
@@ -38,12 +66,12 @@ const Details = () => {
                             <div>
                                 <h3 className="font-medium text-gray-900">Information</h3>
                                 <dl className="mt-2 border-t border-b border-gray-200 divide-y divide-gray-200">
-                                    {/* {Object.keys(currentFile.information).map((key) => (
+                                    {Object.keys(currentFile.information).map((key) => (
                                         <div key={key} className="py-3 flex justify-between text-sm font-medium">
                                             <dt className="text-gray-500">{key}</dt>
                                             <dd className="text-gray-900">{currentFile.information[key]}</dd>
                                         </div>
-                                    ))} */}
+                                    ))}
                                 </dl>
                             </div>
                             <div>
