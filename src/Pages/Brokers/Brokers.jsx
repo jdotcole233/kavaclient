381a1a2b2cb0120a8e2b621b50037363c571eeaf
Khoby-790 <ebaidoo79@gmail.com>
2021-07-06 15:34:20 +0000

    Modified src/Pages/Brokers/Brokers.jsx

--- src/Pages/Brokers/Brokers.jsx
+++ src/Pages/Brokers/Brokers.jsx
@@ -12,7 +12,22 @@ const Brokers = () => {
           <div></div>
           <div className="flex shadow-md rounded-sm">
             <input type="text" className="p-3" />
-            <div className="bg-gray-400"></div>
+            <div className="bg-gray-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </div>
           </div>
         </div>
       </PageWrapper>
