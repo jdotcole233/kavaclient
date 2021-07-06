1d93ca071fdb4dbf4cc6f1cf782ce972b1e22c2b
Khoby-790 <ebaidoo79@gmail.com>
2021-07-06 16:02:19 +0000

    Modified src/Pages/Brokers/Components/OffsetSelector.jsx

--- src/Pages/Brokers/Components/OffsetSelector.jsx
+++ src/Pages/Brokers/Components/OffsetSelector.jsx
@@ -12,7 +12,7 @@ function OffsetSelector() {
       {({ open }) => (
         <>
           <div>
-            <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-green-500">
+            <Menu.Button className="inline-flex justify-center w-full rounded-none border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-green-500">
               {numberOfRows} rows
               <ChevronDownIcon
                 className="-mr-1 ml-2 h-5 w-5"
