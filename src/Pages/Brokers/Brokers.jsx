1563e744781e95964daf2951d9c4f356f09243aa
Khoby-790 <ebaidoo79@gmail.com>
2021-07-06 15:56:19 +0000

    Modified src/Pages/Brokers/Brokers.jsx

--- src/Pages/Brokers/Brokers.jsx
+++ src/Pages/Brokers/Brokers.jsx
@@ -36,6 +36,10 @@ const Brokers = () => {
             </div>
           </div>
         </div>
+
+        <div>
+          <ul className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8"></ul>
+        </div>
       </PageWrapper>
     </BrokerProvider>
   );
