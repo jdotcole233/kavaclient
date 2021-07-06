bd46c71c7840a33f1ee9f73df880d3cad1364264
Khoby-790 <ebaidoo79@gmail.com>
2021-07-06 15:52:20 +0000

    Modified src/Pages/Brokers/Components/OffsetSelector.jsx

--- src/Pages/Brokers/Components/OffsetSelector.jsx
+++ src/Pages/Brokers/Components/OffsetSelector.jsx
@@ -1,18 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import { classNames } from "../../../utils";
 
 function OffsetSelector() {
+  const [numberOfRows, setNumberOfRows] = useState(12);
   return (
     <Menu as="div" className="relative inline-block text-left">
       {({ open }) => (
         <>
           <div>
             <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-green-500">
-              Options
+              {numberOfRows} rows
               <ChevronDownIcon
                 className="-mr-1 ml-2 h-5 w-5"
                 aria-hidden="true"
@@ -38,6 +39,7 @@ function OffsetSelector() {
               <Menu.Item>
                 {({ active }) => (
                   <div
+                    onClick={() => setNumberOfRows(12)}
                     className={classNames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                       "block px-4 py-2 text-sm"
@@ -50,6 +52,7 @@ function OffsetSelector() {
               <Menu.Item>
                 {({ active }) => (
                   <div
+                    onClick={() => setNumberOfRows(12)}
                     className={classNames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                       "block px-4 py-2 text-sm"
@@ -64,6 +67,7 @@ function OffsetSelector() {
               <Menu.Item>
                 {({ active }) => (
                   <div
+                    onClick={() => setNumberOfRows(12)}
                     className={classNames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                       "block px-4 py-2 text-sm"
