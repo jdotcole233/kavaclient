ac9e3d6f36af7ce15b20666ec401d23162d2af2a
Khoby-790 <ebaidoo79@gmail.com>
2021-07-08 02:11:52 +0000

    Modified .firebase/hosting.YnVpbGQ.cache,Modified src/utils.js

--- src/utils.js
+++ src/utils.js
@@ -25,7 +25,7 @@ export const navigation = [
     // { name: 'Home', href: '/', icon: HomeIcon, current: false },
     { name: 'All Offers', href: '/offers', icon: ViewGridIconOutline, current: false },
     { name: 'All Treaties', href: '/treaties', icon: CollectionIcon, current: false },
-    { name: 'Brokers', href: '/brokers', icon: UserGroupIcon, current: false },
+    // { name: 'Brokers', href: '/brokers', icon: UserGroupIcon, current: false },
     // { name: 'Albums', href: '#', icon: PhotographIcon, current: false },
     { name: 'Settings', href: '/settings', icon: CogIcon, current: false },
 ]
