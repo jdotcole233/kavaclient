20a92b0286d96aac992a6c77ebcbeb869cf4b653
Khoby-790 <ebaidoo79@gmail.com>
2021-07-08 04:41:29 +0000

    Modified src/Pages/Offers/Previews/ClosingSlip.jsx

--- src/Pages/Offers/Previews/ClosingSlip.jsx
+++ src/Pages/Offers/Previews/ClosingSlip.jsx
@@ -1,14 +1,16 @@
 import React, { Fragment } from "react";
+import { useLayoutProps } from "../../../layout/Provider/LayoutProvider";
 import IRiskClosingSlip from "./IRiskClosingSlip";
 import KEKClosingSlip from "./KEKClosingSlip";
 import VisalClosingSlip from "./VisalClosinfSlip";
 
 const ClosingSlip = () => {
+  const { activeTab } = useLayoutProps();
   return (
     <Fragment>
-      <KEKClosingSlip />
-      <VisalClosingSlip />
-      {/* <IRiskClosingSlip /> */}
+      {activeTab === 1 && <KEKClosingSlip />}
+      {activeTab === 0 && <VisalClosingSlip />}
+      {activeTab === 2 && <IRiskClosingSlip />}
     </Fragment>
   );
 };
