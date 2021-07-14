898f9741d7f826b0ca79230d7d79cb1da6f56a52
Khoby-790 <ebaidoo79@gmail.com>
2021-07-14 23:57:17 +0000

    Modified src/layout/components/Content.jsx

--- src/layout/components/Content.jsx
+++ src/layout/components/Content.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Brokers from "../../Pages/Brokers/Brokers";
 import Offers from "../../Pages/Offers/Offers";
 import Settings from "../../Pages/Settings/Settings";
@@ -9,10 +9,12 @@ const Content = () => {
   return (
     <Fragment>
       <main className="flex-1 overflow-y-auto">
-        <Route path="/offers" exact component={Offers} />
-        <Route path="/treaties" exact component={Treaties} />
-        <Route path="/settings" exact component={Settings} />
-        <Route path="/brokers" exact component={Brokers} />
+        <Switch>
+          <Route path="/offers" exact component={Offers} />
+          <Route path="/treaties" exact component={Treaties} />
+          <Route path="/settings" exact component={Settings} />
+          <Route path="/brokers" exact component={Brokers} />
+        </Switch>
       </main>
     </Fragment>
   );
