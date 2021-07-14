824a3714bf793f55845c136bbb55487ac7468440
Khoby-790 <ebaidoo79@gmail.com>
2021-07-14 23:37:16 +0000

    Modified src/layout/Auth.jsx

--- src/layout/Auth.jsx
+++ src/layout/Auth.jsx
@@ -2,7 +2,7 @@
 function Auth() {
   return (
     <div className="min-h-screen bg-white flex">
-      <div className="hidden lg:block bg-green-500  relative w-0 flex-1">
+      <div className="hidden lg:block bg-white  relative w-0 flex-1">
         {/* <img
           className="absolute inset-0 h-full w-full object-cover"
           src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
@@ -17,7 +17,7 @@ function Auth() {
           </svg>
         </div>
       </div>
-      <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+      <div className="flex-1 bg-green-700 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <div>
             <img
