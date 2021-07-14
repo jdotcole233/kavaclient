7aa89afe292ab6dff700f17c580f643621364582
Khoby-790 <ebaidoo79@gmail.com>
2021-07-14 23:27:17 +0000

    Modified src/App.js,Modified src/layout/Auth.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Admin from './layout/Admin';
+import Auth from './layout/Auth';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Admin} />
+        <Route path="/" exact component={Admin} />
+        <Route path="/auth" component={Auth} />
       </Switch>
     </BrowserRouter>
   );
