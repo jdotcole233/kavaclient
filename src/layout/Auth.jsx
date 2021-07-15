f54e742910a924eb14987e32d8e7404a0e0b9fa2
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 20:28:46 +0000

    Modified src/layout/Auth.jsx

--- src/layout/Auth.jsx
+++ src/layout/Auth.jsx
@@ -70,7 +70,7 @@ function Auth() {
                   <input
                     type="email"
                     // name="email"
-                    ref={register("email", { required: "Required" })}
+                    {...register("email", { required: "Required" })}
                     id="name"
                     className="block w-full border-0 p-0 mt-2 text-gray-900 placeholder-gray-600 focus:ring-0 focus:outline-none"
                     placeholder="Email here ... eg johndpe@gmail.com"
