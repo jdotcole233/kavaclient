df74b3876e8feebe0104850b7c24766bb6900d46
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 16:48:15 +0000

    Modified src/Pages/Dashboard/Dashboard.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PageHeader from "../../Components/PageHeader";
 import PageWrapper from "../../Components/PageWrapper";
+import DashboardStats from "./components/DashboardStats";
 
 const Dashboard = () => {
   return (
     <PageWrapper>
       <PageHeader title="Dashboard" />
+      <DashboardStats />
     </PageWrapper>
   );
 };
