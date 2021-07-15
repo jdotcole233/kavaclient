b9d77908a964361366158b012c1712b308039353
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 15:22:17 +0000

    Modified src/layout/Auth.jsx

--- src/layout/Auth.jsx
+++ src/layout/Auth.jsx
@@ -44,7 +44,7 @@ function Auth() {
                 <div className="border border-green-600 bg-white rounded-md px-3 py-3 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
                   <label
                     htmlFor="name"
-                    className="block text-sm font-bold text-gray-900"
+                    className="block text-lg font-bold text-gray-900"
                   >
                     Name
                   </label>
@@ -52,8 +52,8 @@ function Auth() {
                     type="text"
                     name="name"
                     id="name"
-                    className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
-                    placeholder="Jane Doe"
+                    className="block w-full border-0 p-0 mt-2 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                    placeholder="Email here ... eg johndpe@gmail.com"
                   />
                 </div>
 
