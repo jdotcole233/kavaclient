845eeeaf9b829f89415d13367b995e31e236367e
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 16:54:17 +0000

    Modified src/Components/PageWrapper.jsx

--- src/Components/PageWrapper.jsx
+++ src/Components/PageWrapper.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const PageWrapper = ({ children }) => {
-  return (
-    <div className="pt-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      {children}
-    </div>
-  );
+  return <div className="pt-8  mx-auto px-4 sm:px-6 lg:px-20">{children}</div>;
 };
 
 export default PageWrapper;
