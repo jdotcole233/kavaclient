cc107d0e5145083ffc39b9e7a805b1b9e182f588
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 17:24:21 +0000

    Modified src/layout/Auth.jsx

--- src/layout/Auth.jsx
+++ src/layout/Auth.jsx
@@ -26,7 +26,7 @@ function Auth() {
             src={
               "https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
             }
-            className="absolute inset-0 h-full w-full object-cover sliderimg"
+            className="absolute inset-0 h-screen w-full object-cover sliderimg"
             alt=""
           />
           {/* <img src={image3} className="sliderimg" alt="" />
