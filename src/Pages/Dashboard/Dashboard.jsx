4b0bc9171d3ff8fc277e69677394894e32905cf0
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 21:11:46 +0000

    Modified src/Pages/Dashboard/Dashboard.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -9,7 +9,10 @@ const Dashboard = () => {
     <PageWrapper>
       <PageHeader title="Dashboard" />
       <div className="flex w-full mt-4">
-        <div className="w-full bg-white shadow-md p-4 rounded">Hello</div>
+        <div className="w-full bg-white shadow-md p-4 rounded">
+          <div className="w-32 h-32 bg-gray-500 rounded-full"></div>
+          <div></div>
+        </div>
         <div className="w-1/2 bg-green-700 shadow-md rounded ml-5"></div>
       </div>
       <DashboardStats />
