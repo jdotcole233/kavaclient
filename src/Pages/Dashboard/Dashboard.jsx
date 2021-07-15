3a011c83ec0c3dc2ffc4750c86f38404f6924cbe
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 16:50:16 +0000

    Added src/Pages/Dashboard/components/DashboardStatsLayer2.jsx,Modified src/Pages/Dashboard/Dashboard.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import PageHeader from "../../Components/PageHeader";
 import PageWrapper from "../../Components/PageWrapper";
 import DashboardStats from "./components/DashboardStats";
+import DashboardStatsLayer2 from "./components/DashboardStatsLayer2";
 
 const Dashboard = () => {
   return (
     <PageWrapper>
       <PageHeader title="Dashboard" />
       <DashboardStats />
+      <DashboardStatsLayer2 />
     </PageWrapper>
   );
 };
