6df259dc9279646ac528e91d6e0e90011024624b
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 15:18:16 +0000

    Modified src/layout/Auth.jsx

--- src/layout/Auth.jsx
+++ src/layout/Auth.jsx
@@ -26,7 +26,7 @@ function Auth() {
           <div className="mt-8">
             <div className="mt-6">
               <form action="#" method="POST" className="space-y-6">
-                <div>
+                {/* <div>
                   <label htmlFor="email" className="block text-sm text-white">
                     Email address
                   </label>
@@ -40,6 +40,21 @@ function Auth() {
                       className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
                     />
                   </div>
+                </div> */}
+                <div className="border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+                  <label
+                    htmlFor="name"
+                    className="block text-xs font-medium text-gray-900"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="block w-full bg-transparent border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+                    placeholder="Jane Doe"
+                  />
                 </div>
 
                 {/* <div className="space-y-1">
