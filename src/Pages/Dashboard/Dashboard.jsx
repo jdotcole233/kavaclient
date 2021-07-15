8a976dbe3e914f3d7b52b2c3a7d18ba679d69258
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 21:12:47 +0000

    Modified src/Pages/Dashboard/Dashboard.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -9,9 +9,13 @@ const Dashboard = () => {
     <PageWrapper>
       <PageHeader title="Dashboard" />
       <div className="flex w-full mt-4">
-        <div className="w-full bg-white shadow-md p-4 rounded">
+        <div className="w-full flex bg-white shadow-md p-4 rounded">
           <div className="w-32 h-32 bg-gray-500 rounded-full"></div>
-          <div></div>
+          <div className="mx-11">
+            <span className="text-gray-700 font-medium">
+              Full name of Reinsurer
+            </span>
+          </div>
         </div>
         <div className="w-1/2 bg-green-700 shadow-md rounded ml-5"></div>
       </div>
