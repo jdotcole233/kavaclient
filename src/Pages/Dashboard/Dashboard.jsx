b1a5aed64513c3c42b9762200c83eebfdbdc4fff
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 21:18:47 +0000

    Modified src/Pages/Dashboard/Dashboard.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -11,11 +11,11 @@ const Dashboard = () => {
       <div className="flex lg:flex-row flex-col w-full mt-4">
         <div className="lg:w-full w-full flex lg:flex-row flex-col bg-white shadow-md p-4 rounded">
           <div className="w-32 h-32 bg-gray-500 rounded-full"></div>
-          <div className="mx-11">
+          <div className="lg:mx-11">
             <span className="text-gray-700 font-medium text-3xl">
               Vanguard Assurance
             </span>
-            <div className="grid mt-3 grid-cols-2">
+            <div className="grid mt-3 grid-cols-1 lg:grid-cols-2">
               <span>Accra</span>
               <span>vanguardassurance@mail.com</span>
               <span>Ghana</span>
