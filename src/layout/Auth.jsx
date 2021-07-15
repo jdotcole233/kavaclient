f9c83cedb141d2196cbc934a60e09ca47c6f38fc
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 17:23:20 +0000

    Modified src/layout/Auth.jsx

--- src/layout/Auth.jsx
+++ src/layout/Auth.jsx
@@ -16,7 +16,7 @@ function Auth() {
       <div className="hidden lg:block bg-white  relative w-0 flex-1">
         <AliceCarousel autoPlay autoPlayInterval="3000">
           <img
-            className="absolute inset-0 h-full w-full object-cover"
+            className="h-screen w-full  sliderimg"
             src={
               "https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
             }
