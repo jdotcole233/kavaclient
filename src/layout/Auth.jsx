0f00633fb357d0fcbccdd35b2ea046b1d95ff9bb
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 15:21:17 +0000

    Modified src/layout/Auth.jsx

--- src/layout/Auth.jsx
+++ src/layout/Auth.jsx
@@ -41,10 +41,10 @@ function Auth() {
                     />
                   </div>
                 </div> */}
-                <div className="border border-green-600 rounded-md px-3 py-3 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+                <div className="border border-green-600 bg-white rounded-md px-3 py-3 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
                   <label
                     htmlFor="name"
-                    className="block text-sm font-medium text-gray-900"
+                    className="block text-sm font-bold text-gray-900"
                   >
                     Name
                   </label>
@@ -52,7 +52,7 @@ function Auth() {
                     type="text"
                     name="name"
                     id="name"
-                    className="block w-full bg-transparent border-0 p-0 text-gray-900 placeholder-gray-100 focus:ring-0 sm:text-sm"
+                    className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
                     placeholder="Jane Doe"
                   />
                 </div>
