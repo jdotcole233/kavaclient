f93414402c4b448d8b5b7fcb93f9c7643678ce8e
Khoby-790 <ebaidoo79@gmail.com>
2021-07-15 15:03:16 +0000

    Modified src/layout/Auth.jsx

--- src/layout/Auth.jsx
+++ src/layout/Auth.jsx
@@ -92,11 +92,11 @@ function Auth() {
                 <div>
                   <button
                     type="submit"
-                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
+                      className="h-5 w-5 mr-5"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                     >
