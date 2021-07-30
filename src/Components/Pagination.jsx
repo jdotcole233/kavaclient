97e24fed6fd18b89def61b63fa8e9d8cead4fc90
Khoby-790 <ebaidoo79@gmail.com>
2021-07-30 10:07:16 +0000

    Modified src/Components/Pagination.jsx

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -67,6 +67,7 @@ const Pagination = ({ data, RenderComponent, title, pageLimit, dataLimit }) => {
             >
               <button
                 onClick={goToPreviousPage}
+                disabled={currentPage === 1}
                 class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
               >
                 <span class="sr-only">Previous</span>
@@ -89,52 +90,17 @@ const Pagination = ({ data, RenderComponent, title, pageLimit, dataLimit }) => {
               {getPaginationGroup().map((item, index) => (
                 <button
                   key={index}
+                  onClick={changePage}
                   className={classNames(
                     currentPage === item
                       ? "z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
                       : "bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
                   )}
                 >
-                  1
+                  {item}
                 </button>
               ))}
-              <a
-                href="#"
-                class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-              >
-                2
-              </a>
-              <a
-                href="#"
-                class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
-              >
-                3
-              </a>
-              <span class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
-                ...
-              </span>
-              <a
-                href="#"
-                class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
-              >
-                8
-              </a>
-              <a
-                href="#"
-                class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-              >
-                9
-              </a>
-              <a
-                href="#"
-                class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-              >
-                10
-              </a>
-              <a
-                href="#"
-                class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-              >
+              <button class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
                 <span class="sr-only">Next</span>
                 {/* <!-- Heroicon name: solid/chevron-right --> */}
                 <svg
@@ -150,7 +116,7 @@ const Pagination = ({ data, RenderComponent, title, pageLimit, dataLimit }) => {
                     clip-rule="evenodd"
                   />
                 </svg>
-              </a>
+              </button>
             </nav>
           </div>
         </div>
