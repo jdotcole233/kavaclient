d06894112cb1dc431ad956ea37bb837ac2fdc9a4
Khoby-790 <ebaidoo79@gmail.com>
2021-07-30 10:08:16 +0000

    Modified src/Components/Pagination.jsx

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -33,47 +33,47 @@ const Pagination = ({ data, RenderComponent, title, pageLimit, dataLimit }) => {
   return (
     <Fragment>
       {/* <!-- This example requires Tailwind CSS v2.0+ --> */}
-      <div class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-        <div class="flex-1 flex justify-between sm:hidden">
+      <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+        <div className="flex-1 flex justify-between sm:hidden">
           <a
-            href="#"
-            class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            // href="#"
+            className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
           >
             Previous
           </a>
           <a
             href="#"
-            class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
           >
             Next
           </a>
         </div>
-        <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+        <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
           <div>
-            <p class="text-sm text-gray-700">
+            <p className="text-sm text-gray-700">
               Showing
-              <span class="font-medium">1</span>
+              <span className="font-medium">1</span>
               to
-              <span class="font-medium">10</span>
+              <span className="font-medium">10</span>
               of
-              <span class="font-medium">97</span>
+              <span className="font-medium">97</span>
               results
             </p>
           </div>
           <div>
             <nav
-              class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+              className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
               aria-label="Pagination"
             >
               <button
                 onClick={goToPreviousPage}
                 disabled={currentPage === 1}
-                class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
               >
-                <span class="sr-only">Previous</span>
+                <span className="sr-only">Previous</span>
                 {/* <!-- Heroicon name: solid/chevron-left --> */}
                 <svg
-                  class="h-5 w-5"
+                  className="h-5 w-5"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                   fill="currentColor"
@@ -100,11 +100,15 @@ const Pagination = ({ data, RenderComponent, title, pageLimit, dataLimit }) => {
                   {item}
                 </button>
               ))}
-              <button class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-                <span class="sr-only">Next</span>
+              <button
+                onClick={goToNextPage}
+                disabled={currentPage === pages}
+                className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                <span className="sr-only">Next</span>
                 {/* <!-- Heroicon name: solid/chevron-right --> */}
                 <svg
-                  class="h-5 w-5"
+                  className="h-5 w-5"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                   fill="currentColor"
