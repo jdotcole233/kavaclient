fa80a7201c7e6a4cfd8d213866c8874cd7bf06e7
Khoby-790 <ebaidoo79@gmail.com>
2021-07-30 10:24:16 +0000

    Modified src/Pages/ConfirmAuth/ConfirmAuth.jsx

--- src/Pages/ConfirmAuth/ConfirmAuth.jsx
+++ src/Pages/ConfirmAuth/ConfirmAuth.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Redirect } from "react-router-dom";
 import { useAuthProcessProps } from "../../layout/Provider/AuthProcessProvider";
+import Comfi from "../../assets/comfi 4.svg";
 
 const ConfirmAuth = () => {
   const { company } = useAuthProcessProps();
   if (!company) return <Redirect to="/auth" />;
-  return <div></div>;
+  return (
+    <Fragment>
+      <div>
+        <img className="h-24 w-auto" src={Comfi} alt="Workflow" />
+        <Fragment>
+          <h2 className="mt-6 text-3xl font-poppins font-extrabold text-gray-900">
+            Welcome, {company?.company_name} {}
+          </h2>
+          <div className="bg-orange-600 text-green-800 p-4">
+            <span className={"text-gray-800 font-poppins"}>
+              Please check your mail for a provided passcode to enable you login
+              to your account
+            </span>
+          </div>
+        </Fragment>
+      </div>
+    </Fragment>
+  );
 };
 
 export default ConfirmAuth;
