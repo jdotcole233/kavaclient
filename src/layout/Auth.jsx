29012114a9e3feea5c8271ced8561ca56fd2a057
Khoby-790 <ebaidoo79@gmail.com>
2021-08-01 17:33:16 +0000

    Modified src/layout/Auth.jsx

--- src/layout/Auth.jsx
+++ src/layout/Auth.jsx
@@ -1,24 +1,23 @@
-import { Fragment, useState } from "react";
-import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { useState } from "react";
+import { Route, Switch } from "react-router-dom";
 import SSL from "../assets/ssl-big.png";
 import Login from "../Pages/Login/Login";
 import Otp from "../Pages/Otp/Otp";
 import AuthProcessProvider from "./Provider/AuthProcessProvider";
-import Comfi from "../assets/comfi 4.svg";
+// import Comfi from "../assets/comfi 4.svg";
 import ConfirmAuth from "../Pages/ConfirmAuth";
 import SubscriptionExpiry from "../Pages/SubscriptionExpiry";
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 function Auth() {
   const [company, setCompany] = useState(undefined);
-  // const { path } = useRouteMatch();
   return (
     <AuthProcessProvider value={{ company, setCompany }}>
       <div className="min-h-screen bg-white flex">
         <div className="hidden lg:block bg-white  relative w-0 flex-1">
           <img
             className="absolute inset-0 h-full w-full object-cover"
-            src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+            src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=contain&w=1908&q=80"
             alt=""
           />
         </div>
