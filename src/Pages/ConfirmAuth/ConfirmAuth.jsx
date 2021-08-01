f723b82b381881663df2f24ca17f8ca4d91fe5eb
Khoby-790 <ebaidoo79@gmail.com>
2021-08-01 17:21:17 +0000

    Modified src/Pages/ConfirmAuth/ConfirmAuth.jsx

--- src/Pages/ConfirmAuth/ConfirmAuth.jsx
+++ src/Pages/ConfirmAuth/ConfirmAuth.jsx
@@ -19,7 +19,7 @@ const ConfirmAuth = () => {
           <div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-11 w-11"
+              className="h-11 w-11 text-gray-500"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -33,7 +33,8 @@ const ConfirmAuth = () => {
             </svg>
           </div>
           <div>
-            <span>{}</span>
+            <span>{company?.company_name}</span>
+            <span>{company?.email}</span>
           </div>
         </div>
       </div>
