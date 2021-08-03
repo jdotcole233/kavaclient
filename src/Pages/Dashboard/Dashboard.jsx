c3a22069cf0c086e06e61dbfad525be693a44bb1
Khoby-790 <ebaidoo79@gmail.com>
2021-08-03 17:34:08 +0000

    Modified src/Pages/Dashboard/Dashboard.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -8,13 +8,13 @@ const Dashboard = () => {
     <PageWrapper>
       <PageHeader title="Dashboard" />
       <div className="bg-white p-3 flex flex-col">
-        <div className="flex">
+        <div className="flex justify-between">
           <div className="">
             <span>Total Parrticipations</span>
           </div>
           <div className="flex">
             <div className="grid">
-              <div className="bg-gray-300">
+              <div className="bg-gray-50 px-4 border border-green-600">
                 <span>Month</span>
               </div>
             </div>
