7b5987e87d6d453633bdb494c1ea1d46b5435ebc
Khoby-790 <ebaidoo79@gmail.com>
2021-08-03 18:25:09 +0000

    Modified src/Pages/Dashboard/Dashboard.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -9,12 +9,12 @@ const Dashboard = () => {
   return (
     <PageWrapper>
       <PageHeader title="Dashboard" />
-      <div className="flex">
+      <div className="flex items-stretch">
         <div className="w-2/3">
           <TotalParticipations />
         </div>
         <div className="w-1/3 ml-3">
-          <div className="bg-white shadow-md"></div>
+          <div className="bg-white h-auto shadow-md p-3"></div>
         </div>
       </div>
       <TotalRevenue />
