f6e6d50a097808e4cefceb16386f0edb83795356
Khoby-790 <ebaidoo79@gmail.com>
2021-08-03 17:40:09 +0000

    Added src/Pages/Dashboard/components/Totalparticipations/TotalParticipations.jsx,Modified src/utils.js

---
+++ src/Pages/Dashboard/components/Totalparticipations/TotalParticipations.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TotalParticipations = () => {
+  return <div></div>;
+};
+
+export default TotalParticipations;
--- src/utils.js
+++ src/utils.js
@@ -104,3 +104,49 @@ export const TIP = "___tken"
 
 export const getUserInitials = user => `${user?.rep_first_name?.charAt(0)}
               ${user?.rep_last_name?.charAt(0)}`
+
+
+export const data = [
+    {
+        name: 'Jan',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Feb',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Mar',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Apr',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'May',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Jun',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Jul',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
