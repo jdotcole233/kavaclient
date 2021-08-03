3656c1484edb72cab5b8db026a4306983213ea3e
Khoby-790 <ebaidoo79@gmail.com>
2021-08-03 18:13:09 +0000

    Modified src/Pages/Dashboard/Dashboard.jsx,Modified src/Pages/Dashboard/components/TotalRevenue/TotalRevenue.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -3,12 +3,14 @@ import PageHeader from "../../Components/PageHeader";
 import PageWrapper from "../../Components/PageWrapper";
 import { useAdminProps } from "../../layout/Provider/AdminProvider";
 import TotalParticipations from "./components/Totalparticipations/TotalParticipations";
+import TotalRevenue from "./components/TotalRevenue/TotalRevenue";
 
 const Dashboard = () => {
   return (
     <PageWrapper>
       <PageHeader title="Dashboard" />
       <TotalParticipations />
+      <TotalRevenue />
     </PageWrapper>
   );
 };
