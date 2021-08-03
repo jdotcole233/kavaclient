5ad30ba64a731d9f4bf584fbfaeee64977e11846
Khoby-790 <ebaidoo79@gmail.com>
2021-08-03 18:40:09 +0000

    Modified src/Pages/Dashboard/Dashboard.jsx,Modified src/Pages/Dashboard/components/TotalRevenue/TotalRevenue.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -11,13 +11,13 @@ const Dashboard = () => {
       <PageHeader title="Dashboard" />
       <div className="flex items-stretch">
         <div className="w-2/3">
-          <TotalParticipations />
+          <TotalRevenue />
         </div>
         <div className="w-1/3 ml-3">
           <div className="bg-white h-auto shadow-md p-3"></div>
         </div>
       </div>
-      <TotalRevenue />
+      <TotalParticipations />
     </PageWrapper>
   );
 };
--- src/Pages/Dashboard/components/TotalRevenue/TotalRevenue.jsx
+++ src/Pages/Dashboard/components/TotalRevenue/TotalRevenue.jsx
@@ -79,14 +79,14 @@ const TotalRevenue = () => {
                   href="#"
                   class="text-gray-500 hover:text-gray-700 px-3 py-2 font-medium text-sm rounded-md"
                 >
-                  My Account
+                  All
                 </a>
 
                 <a
                   href="#"
                   class="text-gray-500 hover:text-gray-700 px-3 py-2 font-medium text-sm rounded-md"
                 >
-                  Company
+                  Paid
                 </a>
 
                 <a
