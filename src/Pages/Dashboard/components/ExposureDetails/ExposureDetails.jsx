301d0321fb39c1cdfa2881c13d75367aa31ca548
Khoby-790 <ebaidoo79@gmail.com>
2021-08-05 12:16:31 +0000

    Modified src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx

--- src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx
+++ src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx
@@ -3,7 +3,19 @@ import React from "react";
 const ExposureDetails = () => {
   return (
     <div>
-      <div className="h-96 w-full bg-gray-300"></div>
+      <div className="h-96 w-full bg-gray-300">
+        <div className="flex justify-between p-4">
+          <div className=""></div>
+          <div className="">
+            <select name="" className="bg-transparent border w-24 p-2" id="">
+              <option value="USD">USD</option>
+              <option value="GHC">GHC</option>
+              <option value="EUR">EUR</option>
+              <option value="GBP">GBP</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
