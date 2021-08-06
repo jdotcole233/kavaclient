52dc380da98c08ff3654630276d0d1452b1112bd
Khoby-790 <ebaidoo79@gmail.com>
2021-08-06 12:45:38 +0000

    Modified src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx

--- src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx
+++ src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx
@@ -87,20 +87,22 @@ const ExposureDetails = () => {
         <div className="grid gap-4 grid-cols-1 w-full px-4 py-5">
           <div className="bg-white rounded-lg w-full p-3">
             <div className="w-1/5 flex h-full items-center justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-24 w-16 text-gray-400 "
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
-                />
-              </svg>
+              <div className="h-3/4 w-3/4 bg-gray-400 rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-16 w-16 text-gray-400 "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
+                  />
+                </svg>
+              </div>
             </div>
           </div>
           <div className="bg-white p-3"></div>
