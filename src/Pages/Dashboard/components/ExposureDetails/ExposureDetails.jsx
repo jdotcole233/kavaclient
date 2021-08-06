dae28dddc9346571043422d21d282610f55f3a9b
Khoby-790 <ebaidoo79@gmail.com>
2021-08-06 12:49:39 +0000

    Modified src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx

--- src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx
+++ src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx
@@ -86,8 +86,8 @@ const ExposureDetails = () => {
       <div className="flex  bg-gray-600 h-2/4 w-full">
         <div className="grid gap-4 grid-cols-1 w-full px-4 py-5">
           <div className="bg-white rounded-lg w-full p-3">
-            <div className="w-1/5 flex flex-col h-full items-center justify-center">
-              <div className="h-36 w-3/4 bg-gray-200 flex justify-center items-center rounded-full">
+            <div className="w-1/5 flex flex-col h-full items-center justify-start">
+              <div className="h-36 w-36 bg-gray-200 flex justify-center items-center rounded-full">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-16 w-16 text-gray-400 "
