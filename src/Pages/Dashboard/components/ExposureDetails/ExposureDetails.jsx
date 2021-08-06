1b6192375287399844911ff4b90ef4f36c55c05e
Khoby-790 <ebaidoo79@gmail.com>
2021-08-06 11:42:14 +0000

    Modified src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx

--- src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx
+++ src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const ExposureDetails = () => {
   return (
-    <div>
-      <div className="h-96 w-full bg-gray-300">
+    <div className="h-full overflow-y-scroll">
+      <div className="h-2/4 w-full bg-gray-300">
         <div className="flex justify-between p-4">
           <div className="">
             <span className="text-3xl font-medium">
@@ -13,7 +13,7 @@ const ExposureDetails = () => {
           <div className="">
             <select
               name=""
-              className="bg-transparent border w-24 px-4 border-gray-600"
+              className="bg-transparent border w-24 px-4 p-2 border-gray-600"
               id=""
             >
               <option value="USD">USD</option>
