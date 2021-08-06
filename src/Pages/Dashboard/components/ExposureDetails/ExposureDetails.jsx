ee41c4902a690da7a8a3a5dbeca2c17f28b03860
Khoby-790 <ebaidoo79@gmail.com>
2021-08-06 12:56:38 +0000

    Modified src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx

--- src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx
+++ src/Pages/Dashboard/components/ExposureDetails/ExposureDetails.jsx
@@ -126,7 +126,9 @@ const ExposureDetails = () => {
                       d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
                     />
                   </svg>
-                  Total Exposure(USD)
+                  <span className="text-2xl font-medium text-gray-400 mx-2">
+                    Total Exposure ( USD )
+                  </span>
                 </span>
               </div>
             </div>
