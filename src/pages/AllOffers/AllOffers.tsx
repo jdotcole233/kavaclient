af65a978fafa61a9c08911e86a07ea38c8a5c0ba
Khoby-790 <ebaidoo79@gmail.com>
2023-01-24 10:51:54 +0000

    chore: :fire: changes to the profile section

--- src/pages/AllOffers/AllOffers.tsx
+++ src/pages/AllOffers/AllOffers.tsx
@@ -24,7 +24,7 @@ const AllOffers = (props: Props) => {
 
   return (
     <PageWrapper>
-      <PageHeader title="Offers" />
+      <PageHeader title="Facultative Offers" />
       <Overview
         stats={[
           {
